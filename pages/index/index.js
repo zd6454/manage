81fe31e9c81cf127c48ac40af6bc97f0181c9208
zd6454 <645405513@qq.com>
2021-05-22 10:38:06 +0800

    1、通知列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,45 +4,57 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+   list:[
+     {  avatar:'../../icons/inform.png',director_name:'书记',date:'2021-05-22',type:'负责人',name:'第一网格负责人:测试1号->测试2号'},
+     {  avatar:'../../icons/inform.png',director_name:'书记',date:'2021-05-22',type:'最新通报',name:'6月3号全社区体检'},
+     {  avatar:'../../icons/inform.png',director_name:'书记',date:'2021-05-20',type:'活动结束',name:'修建草坪活动已结束'},
+   ],
+   
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  handleevents(events){
+    const{list}=this.data;
+   events.map((item)=>{
+     item.date=item.date.slice(0,10)
+     item.avatar='../../icons/inform.png',
+     item.type='发布活动'
+     list.push(item)
+     return item;
+   })
+   this.setData({list})
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+  getAllEvents(){
+    let that = this;
+   wx.request({
+     url: 'https://wuhanhszl.com:3000/users/activities',
+     method: 'GET',
+     credentials: 'omit',
+     skipErrorHandler: true,
+     params: {
+      pageSize:  5,
+      filter: {  },
+      current: 0,
+     },
+     header: {
+      'Access-Token': wx.getStorageSync('access-token'),
+      'Token-Type': wx.getStorageSync('token-type'),
+      Client: wx.getStorageSync('client'),
+      Expiry: wx.getStorageSync('expiry'),
+      Uid: wx.getStorageSync('uid'),
+     },
+     success(res){
+       console.log(res)
+      that.handleevents(res.data.data.slice(0,5))
+     }
+   })
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+ onLoad(options){
+  this.getAllEvents()
+ },
+   /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.getAllEvents()
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
